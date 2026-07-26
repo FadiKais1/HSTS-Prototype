79a0bc22649d933c2313997a56eb7051209021a8
DT <d.t8796@gmail.com>
2026-07-26 19:27:56 +0300

    Add Server control-layer dependencies

--- src/main/java/hsts/server/net/Server.java
+++ src/main/java/hsts/server/net/Server.java
@@ -2,10 +2,26 @@ package hsts.server.net;
 
 import hsts.common.Request;
 import hsts.common.Response;
+import hsts.server.control.AuthService;
+import hsts.server.control.CourseBotService;
+import hsts.server.control.ExamExecutionService;
+import hsts.server.control.ExamManagementService;
+import hsts.server.control.GradingService;
+import hsts.server.control.NotificationService;
+import hsts.server.control.ReportService;
 
 public class Server {
     private int port;
     private boolean running;
+
+    // RELATIONSHIP-DERIVED: Server dispatches requests to the Control layer.
+    private ExamManagementService examManagementService;
+    private ExamExecutionService examExecutionService;
+    private AuthService authService;
+    private GradingService gradingService;
+    private ReportService reportService;
+    private NotificationService notificationService;
+    private CourseBotService courseBotService;
 
     public void startServer() {
         throw new UnsupportedOperationException(
