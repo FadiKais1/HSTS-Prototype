067336cd04ca07c3a418642195166701c805fe62
DT <d.t8796@gmail.com>
2026-07-26 19:56:09 +0300

    Align question bank boundary with Assignment 3 skeleton

--- src/main/java/hsts/client/boundary/QuestionBankPage.java
+++ src/main/java/hsts/client/boundary/QuestionBankPage.java
@@ -1,11 +1,20 @@
 package hsts.client.boundary;
 
+import hsts.client.net.Client;
+import hsts.common.type.DifficultyLevel;
+import hsts.server.entity.Course;
+import hsts.server.entity.Teacher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class QuestionBankPage extends Application {
+    private Client client;
+    private Teacher currentTeacher;
+    private Course selectedCourse;
+
+    // COMPATIBILITY-ONLY
     private QuestionBankPageController controller;
 
     @Override
@@ -28,5 +37,37 @@ public class QuestionBankPage extends Application {
         if (controller != null) {
             controller.close();
         }
+    }
+
+    public void showQuestionsByCourse(int courseId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void createQuestion(int courseId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void editQuestion(int questionId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void activateQuestion(int questionId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void deactivateQuestion(int questionId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void filterQuestionsByDifficulty(DifficultyLevel difficulty) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void filterQuestionsByTopic(String topic) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
+    }
+
+    public void filterQuestionsBySubject(int subjectId) {
+        throw new UnsupportedOperationException("Not implemented in Assignment 2 skeleton");
     }
 }
